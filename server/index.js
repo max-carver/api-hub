572e712c32c56d70caaca70a9d90793f31848169
max-carver <maxvenas3108@gmail.com>
2024-07-07 20:02:08 +0200

    fixes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ import cors from "cors";
 import apiRoutes from "./routes/apiRoutes.js";
 import dotenv from "dotenv";
 dotenv.config();
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
