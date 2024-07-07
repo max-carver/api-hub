e9fbf61dd43787d094b19428f0b942633a1c425b
max-carver <maxvenas3108@gmail.com>
2024-07-07 19:13:26 +0200

    add documentation directory from insomnia

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./APIDocumentation"));
 app.use(cookieParser());
 app.use(
 	cors({
