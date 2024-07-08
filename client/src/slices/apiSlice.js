b8e87fb7740af594ed8caec1428d69f21fbed613
max-carver <maxvenas3108@gmail.com>
2024-07-08 12:24:43 +0200

    change baseQuery url

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:5000" });
+const baseQuery = fetchBaseQuery({ baseUrl: "pex-api-hub.onrender.com" });
 
 export const apiSlice = createApi({
 	baseQuery,
